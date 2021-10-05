759241a4ed0edf35e0429ae0e3e67012e65cd53a
linaAbukhaled <90338198+linaAbukhaled@users.noreply.github.com>
2021-10-05 12:55:01 +0300

    Requested changes for Verification Screen Done

--- src/Containers/Auth/Verification/styles.ts
+++ src/Containers/Auth/Verification/styles.ts
@@ -7,17 +7,10 @@ export const MainContainer = styled.View`
   align-items: center;
 `;
 
-export const Back = styled.TouchableOpacity``;
-
-export const BackText = styled(MediumText)`
-  text-align: right;
-  color: ${({theme}) => theme.colors.orange};
-`;
-
 export const EnterEmailText = styled(MediumText)`
   color: ${({theme}) => theme.colors.orange};
   margin-top: 45px;
   margin-bottom: 20px;
   align-self: flex-start;
-  padding-left: 25px;
+  margin-left: 25px;
 `;
