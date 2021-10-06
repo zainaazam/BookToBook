dfa5014f76ec671148a144959e0431db2313d8f7
linaAbukhaled <90338198+linaAbukhaled@users.noreply.github.com>
2021-10-06 17:44:07 +0300

    Designed About Screen

--- src/Navigation/StackNavigators/AppStack.tsx
+++ src/Navigation/StackNavigators/AppStack.tsx
@@ -3,21 +3,24 @@ import {StyleSheet} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {View} from 'react-native';
 import Home from '../../Containers/AppStack/Home';
+import About from '../../Containers/AppStack/About';
 
 export type AppStackParamList = {
   Home: undefined;
+  About: undefined;
 };
 
 const AppStack = () => {
   const {Navigator, Screen} = createStackNavigator<AppStackParamList>();
   return (
-    <View style={styles.icon}>
+    <View style={styles.screen}>
       <Navigator
         screenOptions={{
           headerShown: false,
         }}
         initialRouteName="Home">
         <Screen name="Home" component={Home} />
+        <Screen name="About" component={About} />
       </Navigator>
     </View>
   );
@@ -26,7 +29,7 @@ const AppStack = () => {
 export default AppStack;
 
 export const styles = StyleSheet.create({
-  icon: {
+  screen: {
     flex: 1,
   },
 });
