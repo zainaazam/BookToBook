cf829fd09a0fefc87e212fbbc026d36092878292
linaAbukhaled <90338198+linaAbukhaled@users.noreply.github.com>
2021-10-13 20:20:33 +0300

    Designed Cgange PassWord Screen

--- src/Containers/Drawer/ChangePassword/index.tsx
+++ src/Containers/Drawer/ChangePassword/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import Button from '../../../Components/Button';
+import CustomHeader from '../../../Components/CustomHeader';
+import CustomModal from '../../../Components/CustomModal';
+import TextField from '../../../Components/TextField';
+import {MainContainer} from './styles';
 
 const ChangePassword = () => {
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
-    <View>
-      <Text>ChangePassword</Text>
-    </View>
+    <MainContainer>
+      <CustomHeader menu title={'ChangePassword'} backButton />
+      <TextField marginTop={50} placeHolder={'Old Password'} />
+      <TextField marginTop={25} placeHolder={'New Password'} />
+      <TextField marginTop={25} placeHolder={'Confirm Password'} />
+      <Button title={'Save'} marginTop={60} onPress={toggleModal} />
+      <CustomModal
+        showModal={showModal}
+        hideModal={toggleModal}
+        message={'Your Password Has Been Successfully Changed!'}
+      />
+    </MainContainer>
   );
 };
 
---
+++ src/Containers/Drawer/ChangePassword/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+
+export const MainContainer = styled.View`
+  background-color: ${({theme}) => theme.colors.backgroundGray};
+  flex: 1;
+  align-items: center;
+`;
